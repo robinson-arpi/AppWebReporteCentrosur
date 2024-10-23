dceff153feb5ae9f889488ec27c051bdb4ae4e1b
robinson-arpi <robinson.arpi@gmail.com>
2024-10-23 13:07:30 -0500

    FIX strcuture in json

--- utils/json_creation.py
+++ utils/json_creation.py
@@ -10,28 +10,30 @@ def convert_timestamps_to_string(df):
             df[column] = df[column].astype(str)
         return df
     except Exception as e:
-            raise Exception(f"Error (convert_timestamps_to_string): {e}")
+        raise Exception(f"Error (convert_timestamps_to_string): {e}")
 
 # Función para guardar el DataFrame como un archivo JSON
 def get_df_as_json(sheet_dfs):
     try:
-        # Crear un diccionario que contendrá todas las hojas
-        all_data = {}
+        # Inicializamos una lista para los registros
+        all_data = []
 
-        for sheet_name, df in sheet_dfs.items():
+        for index, (sheet_name, df) in enumerate(sheet_dfs.items(), start=1):
             # Limpiar y convertir tipos
             df = df.where(pd.notnull(df), None)
             df['hora_inicio'] = df['hora_inicio'].astype(str)
             df['hora_final'] = df['hora_final'].astype(str)
             df['dia'] = df['dia'].astype(str)
 
-            # Convertir el DataFrame a un diccionario y luego a JSON
-            all_data[sheet_name] = df.to_dict(orient='records')
-     
+            # Agregar las columnas num_sheet y nombre_sheet
+            df['numero_sheet'] = index  # Número de la hoja
+            df['nombre_sheet'] = sheet_name.strip()  # Eliminar espacios en blanco
+
+            # Convertir el DataFrame a un diccionario y agregarlo directamente a all_data
+            all_data.extend(df.to_dict(orient='records'))  # Agregar los registros
+
         # Devolver el JSON
-        return all_data
+        return all_data  # Devolver directamente la lista de registros
     
     except Exception as e:
-            raise Exception(f"Error (get_df_as_json): {e}")
-
-        
+        raise Exception(f"Error (get_df_as_json): {e}")
